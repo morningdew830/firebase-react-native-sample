691e2d8565591aee9485f87433d1a5530b99e71a
Ken Kosaki <morningdew830@mail.com>
2017-08-07 03:17:00 +0900

    android build

--- index.android.js
+++ index.android.js
@@ -1,52 +1,130 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Sample Firebase & React Native App
+ * https://github.com/davideast/firebase-react-native-sample
  */
 'use strict';
 
-var React = require('react-native');
-var {
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+const firebase = require('firebase');
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js')
+
+const {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View,
-} = React;
+  TouchableHighlight,
+  Alert,
+} = ReactNative;
 
-var FirebaseReactNativeSample = React.createClass({
-  render: function() {
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCFERl_nsznLl3QR252hLfHcWAmgyOUaWs",
+  authDomain: "chat-app-b99b0.firebaseapp.com",
+  databaseURL: "https://chat-app-b99b0.firebaseio.com",
+  projectId: "chat-app-b99b0",
+  storageBucket: "chat-app-b99b0.appspot.com",
+  messagingSenderId: "807480011980"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class FirebaseReactNativeSample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
       </View>
+    )
+  }
+
+  _addItem() {
+    Alert.prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
     );
   }
-});
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  _renderItem(item) {
+
+    const onPress = () => {
+      Alert.alert(
+        'Confirm',
+        null,
+        [
+          {text: 'Delete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
+  }
+
+}
 
 AppRegistry.registerComponent('FirebaseReactNativeSample', () => FirebaseReactNativeSample);
